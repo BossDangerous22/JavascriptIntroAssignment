8ba51aeb199875ae363173fb238d6d0cf34f45db
BossDangerous22 <106352364+BossDangerous22@users.noreply.github.com>
2024-01-28 12:33:48 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Assign values to variables
+var firstName = "Dionne";
+var lastName = "Campbell";
+var age = 40;  
+
+// Display alerts
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+// Log a message in the console
+console.log("I'm having so much fun in this class!");
